8a6e0698c85e93a47ff73af2e1abfa5325aa7ab7
pixelfreak <pixelfreak2005@googlemail.com>
2017-01-30 16:43:53 +0100

    Update FilmStripView.java

--- src/com/android/camera/ui/FilmStripView.java
+++ src/com/android/camera/ui/FilmStripView.java
@@ -889,6 +889,10 @@ public class FilmStripView extends ViewGroup implements BottomControlsListener {
         return false;
     }
 
+    @Override
+    public boolean shouldDelayChildPressedState() {
+        return !inCameraFullscreen();
+    }
     private int findTheNearestView(int pointX) {
 
         int nearest = 0;
